cd9c689fc5317cd13009b38ebf48820461a9ba5d
MaximZoz <max591018@gmail.com>
2022-04-04 13:31:44 +0200

    add migration

---
+++ Migrations/20220403211552_AddCompletedTodoItemTask.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetClaimAuthorization.Migrations
+{
+    public partial class AddCompletedTodoItemTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "Tasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "Tasks");
+        }
+    }
+}
